85b9e32a02b17392c1495f0b5e0dd37b255543f2
Julio Quina <julio.quina@blueprism.com>
2023-11-21 18:50:56 +0000

    Additional changes to source code

--- app/server.js
+++ app/server.js
@@ -1,23 +1,25 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 let app = express();
+const PORT = 3000;
+const mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (_, res) {
+  const pathToFile = path.join(__dirname, "index.html");
 
-app.get('/profile-picture', function (req, res) {
+  console.log(pathToFile);
+  res.sendFile(pathToFile);
+});
+
+app.get("/profile-picture", function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  res.end(img, "binary");
 });
 
 // use when starting application locally
@@ -26,55 +28,49 @@ let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 // use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "my-db";
+// let databaseName = "user-account";
 
-app.post('/update-profile', function (req, res) {
+app.post("/update-profile", function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-    userObj['userid'] = 1;
-
-    let myquery = { userid: 1 };
+  console.log("Updating profile");
+  // MongoClient.connect(mongoUrlLocal).then((client) => {
+  MongoClient.connect(mongoUrlDocker).then((client) => {
+    const db = client.db(databaseName);
+    userObj["userid"] = 1;
+    const query = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
-
-  });
-  // Send response
-  res.send(userObj);
-});
-
-app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-
-    let myquery = { userid: 1 };
-
-    db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
-      client.close();
-
-      // Send response
-      res.send(response ? response : {});
-    });
+    db.collection("users")
+      .updateOne(query, newvalues, { upsert: true })
+      .then(() => {
+        res.send(userObj);
+      });
   });
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+app.get("/get-profile", function (req, res) {
+  // MongoClient.connect(mongoUrlLocal)
+  MongoClient.connect(mongoUrlDocker)
+    .then((client) => {
+      const db = client.db(databaseName);
+      const query = { userid: 1 };
+
+      db.collection("users")
+        .findOne(query)
+        .then((resp) => {
+          res.send(resp || {});
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+mongoose.connection.on("connected", () => console.log("connected"));
+
+app.listen(PORT, function () {
+  console.log(`app listening on port ${PORT}`);
 });
